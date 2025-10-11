3cba0abdd396070c68ec5342eee1fb9b8bd225ae
tnemnorivnelee <tnemnorivnelee@gmail.com>
2025-10-11 17:11:36 +0900

    style: 기본 테마를 다크 모드에서 라이트 모드로 변경

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -12,7 +12,7 @@ export function ClientLayout({
 }>) {
   return (
     <>
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
         {children}
         <Toaster />
       </ThemeProvider>
